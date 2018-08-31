81a7a4337f030f44625d3de4fa9a2c5215b0cf48
Dustin L <r.dustin.lane@gmail.com>
2018-08-30 20:44:48 -0700

    flask-mailroom - Cleanup main.py

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
+#!/usr/bin/env python3
+"""Main flask app"""
 import os
 import base64
-
 from flask import Flask, render_template, request, redirect, url_for, session
-
-from model import Donation 
+from model import Donation
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return redirect(url_for('all'))
 
-@app.route('/donations/')
+
+@app.route('/all')
 def all():
     donations = Donation.select()
-    return render_template('donations.jinja2', donations=donations)
-    
+    return render_template('donations.html', donations=donations)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
 
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 6738))
+    app.run(host='0.0.0.0', port=port, debug=True)
