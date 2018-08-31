24ea80d7988d5f4a8ac3ef67b152bc09c60238a0
Dustin L <r.dustin.lane@gmail.com>
2018-08-30 20:19:19 -0700

    flask-mailroom - update setup script

--- setup.py
+++ setup.py
@@ -1,26 +1,32 @@
+#!/usr/bin/env python3
+"""Flask-Mailroom setup script"""
 import random
+from model import db, Donor, Donation
 
-from model import db, Donor, Donation 
 
-db.connect()
+def main():
+    """Main function for populating the database"""
+    donors = [('Toni Morrison', random.sample(range(100, 25000), 3)),
+              ('Mike McHargue', random.sample(range(100, 25000), 3)),
+              ("Flannery O'Connor", random.sample(range(100, 25000), 3)),
+              ('Angelina Davis', random.sample(range(100, 25000), 3)),
+              ('Bell Hooks', random.sample(range(100, 25000), 3))]
 
-# This line will allow you "upgrade" an existing database by
-# dropping all existing tables from it.
-db.drop_tables([Donor, Donation])
+    with db:
+        db.execute_sql('PRAGMA foreign_keys = ON;')
+        db.drop_tables([Donor, Donation])
+        db.create_tables([Donor, Donation])
 
-db.create_tables([Donor, Donation])
+        for donor, donations in donors:
+            Donor.create(name=donor,
+                         total_donations=sum(donations),
+                         ave_donations=sum(donations) / len(donations))
 
-alice = Donor(name="Alice")
-alice.save()
+        for donor, donations in donors:
+            for donation in donations:
+                Donation.create(donor=donor,
+                                amount=donation)
 
-bob = Donor(name="Bob")
-bob.save()
 
-charlie = Donor(name="Charlie")
-charlie.save()
-
-donors = [alice, bob, charlie]
-
-for x in range(30):
-    Donation(donor=random.choice(donors), value=random.randint(100, 10000)).save()
-
+if __name__ == '__main__':
+    main()
