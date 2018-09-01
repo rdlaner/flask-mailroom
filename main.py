643a0cab9a39800a701f9d3dc62c8dfcb6fff90d
Dustin L <r.dustin.lane@gmail.com>
2018-08-31 18:57:16 -0700

    flask-mailroom - Add donors and add_donation views

--- main.py
+++ main.py
@@ -2,21 +2,52 @@
 """Main flask app"""
 import os
 import base64
+import decimal
 from flask import Flask, render_template, request, redirect, url_for, session
-from model import Donation
+from model import Donation, Donor
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return redirect(url_for('all'))
+    return redirect(url_for('donations'))
 
 
-@app.route('/all')
-def all():
+@app.route('/donations')
+def donations():
     donations = Donation.select()
     return render_template('donations.html', donations=donations)
+
+
+@app.route('/donors')
+def donors():
+    donors = Donor.select()
+    return render_template('donors.html', donors=donors)
+
+
+@app.route('/add_donation', methods=['GET', 'POST'])
+def add_donation():
+    if request.method == 'POST':
+        input_donor = request.form['donor']
+        input_donation = request.form['donation']
+
+        # Create new donation in database
+        Donation.create(donor=input_donor, amount=input_donation)
+
+        # Assign new donation to donor
+        try:
+            donor = Donor.get(Donor.name == input_donor)
+        except Donor.DoesNotExist:
+            donor = Donor.create(name=input_donor, total=0, average=0)
+
+        donor.total += decimal.Decimal(input_donation)
+        donor.average = donor.total / len(donor.donations)
+        donor.save()
+
+        return redirect(url_for('home'))
+
+    return render_template('add_donation.html')
 
 
 if __name__ == '__main__':
