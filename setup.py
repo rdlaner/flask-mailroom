40428b22cb01995ec38396e9371c898e0f1b8c0d
Dustin L <r.dustin.lane@gmail.com>
2018-08-31 20:29:52 -0700

    flask-mailroom - Remove use of context manager for connecting to db

--- setup.py
+++ setup.py
@@ -13,20 +13,20 @@ def main():
               ('Angelina Davis', random.sample(range(100, 25000), 3)),
               ('Bell Hooks', random.sample(range(100, 25000), 3))]
 
-    with db:
-        db.drop_tables([Donor, Donation])
-        db.create_tables([Donor, Donation])
+    db.connect()
+    db.drop_tables([Donor, Donation])
+    db.create_tables([Donor, Donation])
 
-        for donor, donations in donors:
-            Donor.create(name=donor,
-                         password=pbkdf2_sha256.hash('secret_password'),
-                         total=sum(donations),
-                         average=sum(donations) / len(donations))
+    for donor, donations in donors:
+        Donor.create(name=donor,
+                     password=pbkdf2_sha256.hash('secret_password'),
+                     total=sum(donations),
+                     average=sum(donations) / len(donations))
 
-        for donor, donations in donors:
-            for donation in donations:
-                Donation.create(donor=donor,
-                                amount=donation)
+    for donor, donations in donors:
+        for donation in donations:
+            Donation.create(donor=donor,
+                            amount=donation)
 
 
 if __name__ == '__main__':
